fb4d5def7db4ba5af16037d75ab76c461651511b
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-06-18 16:32:11 -0300

    refactor(rank): change bot message

--- src/commands/wins/rank.ts
+++ src/commands/wins/rank.ts
@@ -6,24 +6,25 @@ export default {
   description: 'Mostra rank de vitórias',
   async execute(client, msg) {
     const rank = await UserRepository.findWinnerRank();
+    const formattedDate = new Intl.DateTimeFormat('pt-BR').format(new Date());
 
     await msg.channel.send(
-      '🏆 Ranking de Vitórias no Fortnite:\n\n'
-        + `🥇 ${
-          rank[0]
-            ? `<@${rank[0].id}> é o melhor! Elu já tem ${rank[0].wins} vitórias!!!`
-            : '---'
-        }\n`
+      `***🏆 RANKING DE VITÓRIAS NO FORTNITE (${formattedDate}):***\n\n`
+      + `🥇 ${
+        rank[0]
+          ? `<@${rank[0].id}> é o(a) melhor! Ele(a) já tem **\`${rank[0].wins} vitórias\`**`
+          : '---'
+      }\n\n`
         + `🥈 ${
           rank[1]
-            ? `<@${rank[1].id}> é bem bom, viu! Ele tem ${rank[1].wins} vitórias`
+            ? `<@${rank[1].id}> é bem bom, viu! Ele(a) tem **\`${rank[1].wins} vitórias\`**`
             : '---'
-        }\n`
+        }\n\n`
         + `🥉 ${
           rank[2]
-            ? `<@${rank[2].id}> é um jogador notável. Conta com ${rank[2].wins} vitórias`
+            ? `<@${rank[2].id}> é um(a) jogador(a) notável. Conta com **\`${rank[2].wins} vitórias\`**`
             : '---'
-        }\n`,
+        }`,
     );
   },
 } as ICommand;
