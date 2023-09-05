ca5d15880c3bdc5c58b1911a7a2d69a8f5c0a68d
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-09-05 14:56:46 -0300

    feat: add support to slash commands

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,11 +1,12 @@
 import { Message } from 'discord.js';
 import { IClient } from './client';
+import { ISlashCommand } from './slashCommand';
 
 export interface ICommand {
   name: string;
   description: string;
   aliases?: string[];
-  execute(client: IClient<ICommand>, msg: Message, args: string[]): Promise<unknown>;
+  execute(client: IClient<ICommand, ISlashCommand>, msg: Message, args: string[]): Promise<unknown>;
 }
 
 export interface ICommandFlag extends ICommand {
