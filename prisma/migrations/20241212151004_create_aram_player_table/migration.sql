5e500165cd2beff97d5b1a9e8169856010f8f533
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-12-12 11:45:32 -0300

    feat: add command to call aram

---
+++ prisma/migrations/20241212151004_create_aram_player_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "AramPlayer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "AramPlayer_pkey" PRIMARY KEY ("id")
+);
---
+++ src/commands/aram.ts
@@ -0,0 +1,21 @@
+import AramPlayerRepository from '@/repositories/AramPlayer.repository';
+import { ICommand } from '@/types/command';
+
+export default {
+  name: 'aram',
+  description: 'Envia foto do ARAM',
+  async execute(client, msg) {
+    const files = [];
+
+    if (process.env.ARAM_IMAGE_URL) {
+      files.push(process.env.ARAM_IMAGE_URL);
+    }
+
+    const aramPlayers = await AramPlayerRepository.findAll();
+
+    msg.channel.send({
+      content: aramPlayers.map((aramPlayer) => `<@${aramPlayer.id}>`).join(' '),
+      files,
+    });
+  },
+} as ICommand;
